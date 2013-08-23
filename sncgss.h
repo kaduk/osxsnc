1917dc7246bb0f134b0dcf81e85f5642d8677972
Ben Kaduk <kaduk@mit.edu>
2013-08-23 14:57:11 -0400

    Grab sapgss_info_s struct definition
    Upstream's header defines the fields of this struct.
    Write it in a neater style.

--- sncgss.h
+++ sncgss.h
@@ -39,6 +39,26 @@
 #ifndef SNCGSS_H
 #define SNCGSS_H
 
+/* SNC Adaptor description, as described in upstream's header. */
+struct sapgss_info_s {
+    int major_rev;
+    int minor_rev;
+    char *adapter_name;
+    SAPGSS_MECH_ID mech_id;
+    char integ_avail;
+    char conf_avail;
+    char unused;
+    char export_sec_context;
+    uint_32_t unused1;
+    sapgss_OID_desc *nt_canonical_name;
+    sapgss_OID_desc *pad[4];
+    char *mech_prefix_string;
+    char mutual_auth;
+    char replay_prot;
+    char reserved[2];
+    sapgss_OID_desc *mech_oid;
+};
+
 /* Copies of GSS structures to get the native-aligned ABI */
 #if 0 /* This should be ABI-compatible with 16-bit and native alignment */
 typedef struct sapgss_buffer_desc_struct {
